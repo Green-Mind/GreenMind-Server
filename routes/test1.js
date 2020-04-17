176cf623d67ec25b5cf280fc0fc26b5791d54af3
Park Yujin <45596615+brillantescene@users.noreply.github.com>
2020-04-18 01:00:15 +0900

    서버 틀

---
+++ routes/test1.js
@@ -0,0 +1,75 @@
+var express = require('express');
+var router = express.Router();
+
+router.use('path', ...);
+
+module.exports = router;
+
+
+var express = require('express');
+var http = require('http');
+var bodyParser= require('body-parser');
+var app = express();
+
+app.set('port',process.env.PORT || 3000);
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+//첫 번째 미들웨어
+app.use(function(req, res, next) {
+
+    console.log('첫 번째 미들웨어 호출');
+    var figure1 ={'internal': 0, 'social': 0, 'external': 0, 'sensation': 0};
+    
+    //activity에서 받아오기
+    var a = [];
+    a[0] = req.body.a1;    
+    a[1] = req.body.a2;
+    a[2] = req.body.a3;
+    a[3] = req.body.a4;
+    a[4] = req.body.a5;
+    a[5] = req.body.a6;
+    a[6] = req.body.a7;
+    a[7] = req.body.a8;
+    a[8] = req.body.a9;
+    a[9] = req.body.a10;
+    a[10] = req.body.a11;
+    a[11] = req.body.a12;
+    a[12] = req.body.a13;
+    a[13] = req.body.a14;
+    a[14] = req.body.a15;
+    a[15] = req.body.a16;
+    a[16] = req.body.a17;
+    a[17] = req.body.a18;
+    a[18] = req.body.a19;
+    a[19] = req.body.a20;
+    a[20] = req.body.a21;
+    a[21] = req.body.a22;
+    a[22] = req.body.a23;
+    a[23] = req.body.a24;
+    
+
+    for(var i=0; i < len(a); i++){
+        var tmp = a[i].split(' '); //internal 1 -> 'internal', '1'
+        tmp[1] *= 1;
+        
+        if(tmp[0].indexOf("internal"))
+            figure1[0] += tmp[1];
+        else if (tmp[0].indexOf("social"))
+            figure1[1] += tmp[1];
+        else if (tmp[0].indexOf("external"))
+            figure1[2] += tmp[1];
+        else
+            figure1[3] += tmp[1];    
+    }
+    
+    console.log('internal_1 : '+figure1[0]+'  social_1 : '+figure1[1]+'  external_1 : '+figure1[2]+'  sensation_1 : '+figure1[3]);
+
+    res.send(figure1); //복수개 보내기 가능한가?
+
+});
+
+
+var server = http.createServer(app).listen(app.get('port'),function(){
+   console.log("익스프레스로 웹 서버를 실행함 : "+ app.get('port')); 
+});
