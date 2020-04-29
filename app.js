483453481f5a8a2eb47ee552261ea606a1f1d804
Park Yujin <45596615+brillantescene@users.noreply.github.com>
2020-04-29 22:10:51 +0900

    수정수정

--- app.js
+++ app.js
@@ -3,8 +3,11 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var bodyParser = require('body-parser');
+
 
 var indexRouter = require('./routes/index');
+var users = require('./routes/users');
 //var test1Router = require('./routes/test1');
 //var test2Router = require('./routes/test2');
 
@@ -12,7 +15,7 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+//app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -21,6 +24,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/users', users);
 //app.use('/test1', test1Router);
 //app.use('/test2', test2Router);
 
--- routes/api/test/test2.js
+++ routes/api/test/test2.js
@@ -1,8 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-//router.use('path', ...);
-
+console.log('trace: /api/test/test2.js');
 var http = require('http');
 var bodyParser= require('body-parser');
 var app = express();
@@ -58,15 +57,16 @@ app.use(function(req, res, next) {
         else
             figure2[3] += tmp[1];    
     }
-    //var paramPassword = req.body.password;
+
     console.log('internal_2 : '+figure2[0]+'  social_2 : '+figure2[1]+'  external_2 : '+figure2[2]+'  sensation_2 : '+figure2[3]);
 
-    res.send(figure2);
+    res.send(figure2[0]);
 
 });
 
+/*
 var server = http.createServer(app).listen(app.get('port'),function(){
    console.log("익스프레스로 웹 서버를 실행함 : "+ app.get('port')); 
 });
-
+*/
 module.exports = router;
